79d69a965b98ecc2edda19232dc3c6df6889d69e
Aaron Yoder <ayoder243@gmail.com>
2023-10-05 10:31:45 -0500

    Get represtatives from Google api based on zipcode
    Retrieves names of represtatives to output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -6,7 +10,8 @@ end
 
 puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv',
+contents = CSV.open(
+  'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
@@ -14,8 +19,17 @@ contents = CSV.open('event_attendees.csv',
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 
-  
-
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislator_names}"
 end
